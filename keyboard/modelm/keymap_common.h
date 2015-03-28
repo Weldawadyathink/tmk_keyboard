9523b79e0c71cb21ee881dfc7a605c3971a082a3
weldawadyathink <spenserb18@gmail.com>
2015-03-27 19:38:09 -0700

    updated files to model m layout

--- keyboard/modelm/keymap_common.h
+++ keyboard/modelm/keymap_common.h
@@ -38,17 +38,19 @@ extern const uint16_t fn_actions[];
  * K2C, K31 and  K3C are extra keys for ISO
  */
 #define KEYMAP( \
-    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, \
-    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, \
-    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, \
-    K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, \
-    K40, K41, K42,           K45,                K49, K4A, K4B, K4C, K4D  \
+    K02, K23, K24, K14, K04, K06, K08, K19, K29, K26, K36, K3B, K3C,      K3F, K4F, K6E,                     \
+    K22, K32, K33, K34, K35, K25, K27, K37, K38, K39, K3A, K2A, K28, K16, K2C, K2E, K2D, K6B, K6C, K6D, K7D, \
+    K12, K42, K43, K44, K45, K51, K17, K47, K48, K49, K4A, K1A, K18, K56, K2B, K3E, K3D, K4B, K4C, K4D, K4E, \
+    K13, K52, K53, K54, K55, K05, K07, K57, K58, K59, K5A, K0A, K66,                     K1B, K1C, K1D,      \
+    K11, K62, K63, K64, K65, K75, K77, K67, K68, K69, K7A, K61,                K0E,      K5B, K5C, K5D, K5E, \
+    K20,      K0F,                K76,                K7F,      K60,      K7E, K7B, K7C, K0C,      K0D       \
 ) { \
-    { KC_##K00, KC_##K01, KC_##K02, KC_##K03, KC_##K04, KC_##K05, KC_##K06, KC_##K07, KC_##K08, KC_##K09, KC_##K0A, KC_##K0B, KC_##K0C, KC_##K0D }, \
-    { KC_##K10, KC_##K11, KC_##K12, KC_##K13, KC_##K14, KC_##K15, KC_##K16, KC_##K17, KC_##K18, KC_##K19, KC_##K1A, KC_##K1B, KC_##K1C, KC_##K1D }, \
-    { KC_##K20, KC_##K21, KC_##K22, KC_##K23, KC_##K24, KC_##K25, KC_##K26, KC_##K27, KC_##K28, KC_##K29, KC_##K2A, KC_##K2B, KC_##K2C, KC_##K2D }, \
-    { KC_##K30, KC_##K31, KC_##K32, KC_##K33, KC_##K34, KC_##K35, KC_##K36, KC_##K37, KC_##K38, KC_##K39, KC_##K3A, KC_##K3B, KC_##K3C, KC_##K3D }, \
-    { KC_##K40, KC_##K41, KC_##K42, KC_NO,    KC_NO,    KC_##K45, KC_NO,    KC_NO,    KC_NO,    KC_##K49, KC_##K4A, KC_##K4B, KC_##K4C, KC_##K4D }  \
+    { KC_##K02, KC_##K23, KC_##K24, KC_##K14, KC_##K04, KC_##K06, KC_##K08, KC_##K19, KC_##K29, KC_##K26, KC_##K36, KC_##K3B, KC_##K3C, KC_NO   , KC_##K3F, KC_##K4F, KC_##K6E, KC_NO   , KC_NO   , KC_NO   , KC_NO    }, \
+    { KC_##K22, KC_##K32, KC_##K33, KC_##K34, KC_##K35, KC_##K25, KC_##K27, KC_##K37, KC_##K38, KC_##K39, KC_##K3A, KC_##K2A, KC_##K28, KC_##K16, KC_##K2C, KC_##K2E, KC_##K2D, KC_##K6B, KC_##K6C, KC_##K6D, KC_##K7D }, \
+    { KC_##K12, KC_##K42, KC_##K43, KC_##K44, KC_##K45, KC_##K51, KC_##K17, KC_##K47, KC_##K48, KC_##K49, KC_##K4A, KC_##K1A, KC_##K18, KC_##K56, KC_##K2B, KC_##K3E, KC_##K3D, KC_##K4B, KC_##K4C, KC_##K4D, KC_##K4E }, \
+    { KC_##K13, KC_##K52, KC_##K53, KC_##K54, KC_##K55, KC_##K07, KC_##K07, KC_##K57, KC_##K58, KC_##K59, KC_##K5A, KC_##K0A, KC_##K66, KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_##K1B, KC_##K1C, KC_##K1D, KC_NO    }, \
+    { KC_##K11, KC_##K62, KC_##K63, KC_##K64, KC_##K65, KC_##K75, KC_##K77, KC_##K67, KC_##K68, KC_##K69, KC_##K7A, KC_##K61, KC_NO   , KC_NO   , KC_NO   , KC_##K0E, KC_NO   , KC_##K5B, KC_##K5C, KC_##K5D, KC_##K5E }, \
+    { KC_##K20, KC_NO   , KC_##K0F, KC_NO   , KC_NO   , KC_NO   , KC_##K76, KC_NO   , KC_NO   , KC_NO   , KC_##K7F, KC_NO   , KC_##K60, KC_NO   , KC_##K7E, KC_##K7B, KC_##K7C, KC_##K0C, KC_NO   , KC_##K0D, KC_NO    }  \
 }
 
 /* ANSI valiant. No extra keys for ISO */
--- keyboard/modelm/keymap_modelm.c
+++ keyboard/modelm/keymap_modelm.c
@@ -2,94 +2,15 @@
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: qwerty */
-    KEYMAP_ANSI(
-        GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
-        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \
-        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,  \
-        LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT, \
-        LCTL,LGUI,LALT,          SPC,                     FN0, RGUI,APP, RCTL),
-    /* 1: colemak */
-    KEYMAP_ANSI(
-        GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
-        TAB, Q,   W,   F,   P,   G,   J,   L,   U,   Y,   SCLN,LBRC,RBRC,BSLS, \
-        BSPC,A,   R,   S,   T,   D,   H,   N,   E,   I,   O,   QUOT,     ENT,  \
-        LSFT,Z,   X,   C,   V,   B,   K,   M,   COMM,DOT, SLSH,          RSFT, \
-        LCTL,LGUI,LALT,          SPC,                     FN0, RGUI,APP, RCTL),
-    /* 2: dvorak */
-    KEYMAP_ANSI(
-        GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   LBRC,RBRC,BSPC, \
-        TAB, QUOT,COMM,DOT, P,   Y,   F,   G,   C,   R,   L,   SLSH,EQL, BSLS, \
-        CAPS,A,   O,   E,   U,   I,   D,   H,   T,   N,   S,   MINS,     ENT,  \
-        LSFT,SCLN,Q,   J,   K,   X,   B,   M,   W,   V,   Z,             RSFT, \
-        LCTL,LGUI,LALT,          SPC,                     FN0, RGUI,APP, RCTL),
-    /* 3: workman */
-    KEYMAP_ANSI(
-        GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
-        TAB, Q,   D,   R,   W,   B,   J,   F,   U,   P,   SCLN,LBRC,RBRC,BSLS, \
-        BSPC,A,   S,   H,   T,   G,   Y,   N,   E,   O,   I,   QUOT,     ENT,  \
-        LSFT,Z,   X,   M,   C,   V,   K,   L,   COMM,DOT, SLSH,          RSFT, \
-        LCTL,LGUI,LALT,          SPC,                     FN0, RGUI,APP, RCTL),
-    /* 4: Poker with Arrow */
-    KEYMAP_ANSI(
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS, \
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          UP,   \
-        TRNS,TRNS,TRNS,          TRNS,                    TRNS,LEFT,DOWN,RGHT),
-    /* 5: Poker with Esc */
-    KEYMAP_ANSI(
-        ESC, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS, \
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          TRNS, \
-        TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS),
-    /* 6: Poker Fn
-     * ,-----------------------------------------------------------.
-     * |Esc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|       |
-     * |-----------------------------------------------------------|
-     * |     |FnQ| Up|   |   |   |   |   |   |Cal|   |Hom|Ins|FnL  |
-     * |-----------------------------------------------------------|
-     * |      |Lef|Dow|Rig|   |   |Psc|Slk|Pau|   |Tsk|End|        |
-     * |-----------------------------------------------------------|
-     * |        |Del|   |Web|Mut|VoU|VoD|   |PgU|PgD|Del|          |
-     * |-----------------------------------------------------------|
-     * |    |    |    |         FnS            |    |    |    |    |
-     * `-----------------------------------------------------------'
-     * Fn:  to Fn overlay
-     * FnL: to Layout selector overaly
-     * FnQ: toggle Esc overlay
-     * FnS: toggle Arrow overlay
-     */
-    KEYMAP_ANSI(
-        ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, TRNS, \
-        TRNS,FN2, UP,  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,CALC,TRNS,HOME,INS, FN4,  \
-        TRNS,LEFT,DOWN,RGHT,TRNS,TRNS,PSCR,SLCK,PAUS,TRNS,FN3, END,      TRNS, \
-        TRNS,DEL, TRNS,WHOM,MUTE,VOLU,VOLD,TRNS,PGUP,PGDN,DEL,           TRNS, \
-        TRNS,TRNS,TRNS,          FN1,                     TRNS,TRNS,TRNS,TRNS),
-    /* 7: Layout selector
-     * ,-----------------------------------------------------------.
-     * | Lq| Lc| Ld| Lw|   |   |   |   |   |   |   |   |   |       |
-     * |-----------------------------------------------------------|
-     * |     |Lq |Lw |   |   |   |   |   |   |   |   |   |   |     |
-     * |-----------------------------------------------------------|
-     * |      |   |   |Ld |   |   |   |   |   |   |   |   |        |
-     * |-----------------------------------------------------------|
-     * |        |   |   |Lc |   |   |   |   |   |   |   |          |
-     * |-----------------------------------------------------------|
-     * |    |    |    |                        |    |    |    |    |
-     * `-----------------------------------------------------------'
-     * Lq: set Qwerty layout
-     * Lc: set Colemak layout
-     * Ld: set Dvorak layout
-     * Lw: set Workman layout
-     */
-    KEYMAP_ANSI(
-        FN5, FN6, FN7, FN8, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
-        TRNS,FN5, FN8, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
-        TRNS,TRNS,TRNS,FN7, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS, \
-        TRNS,TRNS,TRNS,FN6, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          TRNS, \
-        TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS),
+//      |                  MAIN KEYBOARD                                       | |    ARROWS     | |       TENKEY        |
+    KEYMAP(ESC,   F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,       PSCR, SLCK, PAUS,                         \
+           GRV,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, INS,  HOME, PGUP, NLCK, PSLS, PAST, PMNS, \
+           TAB,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, DEL,  END,  PGDN, P7,   P8,   P9,   PPLS, \
+           CAPS,  A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,                         P4,   P5,   P6,         \
+           LSFT,  Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,                 UP,         P1,   P2,   P3,   PENT, \
+           LCTL,       LALT,               SPC,                RALT,     RCTL,      LEFT, DOWN, RGHT, P0,         PDOT),
 };
+
 const uint16_t PROGMEM fn_actions[] = {
     /* Poker Layout */
     [0] = ACTION_LAYER_MOMENTARY(6),  // to Fn overlay
